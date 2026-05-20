062d90a5c9f6c5dcec135e792fe19f1897510c0d
Dark-303 <156324532+Dark-303@users.noreply.github.com>
2026-05-20 18:30:37 -0400

    Pathogen Data

---
+++ Assets/Scripts/Data/PathogenData.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewPathogen", menuName = "Enemies/Pathogen")]
+public class PathogenData : ScriptableObject
+{
+    [Header("Biological Stats")]
+    public string pathogenName;
+    public int maxHP;
+    public int maxDamage;
+    public Sprite visualSprite;
+
+    // TODO: Add smthsmth antigens
+}
